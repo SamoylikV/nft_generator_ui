0df51e226ff1ac50b4b3196494aa702fc9908026
ToxicVasiliy <44618834+ToxicVasiliy@users.noreply.github.com>
2022-02-04 05:15:09 +0500

    blyat im gonna die
    щас умру пиздец блять

--- ui.py
+++ ui.py
@@ -1,26 +1,26 @@
 import sys
 import ui_pyqt
-import os
 from PyQt5 import QtWidgets
-import shutil
-import tkinter as tk
-from tkinter import filedialog
-from commands import install_, build_, remove_layer_, add_layer_, add_file_, remove_file_, select_mode_, change_rarity
+from commands import install_, build_, remove_layer_, add_layer_, add_file_, remove_file_, select_mode_, change_rarity_
 
 
-class doctors_nft(QtWidgets.QMainWindow, ui_pyqt.Ui_MainWindow):
+class doctors_nft(QtWidgets.QMainWindow, ui_pyqt.Ui_DartDocotr_nft_genrator):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.layer_name = ''
+        self.rarity = ''
 
         self.enter.clicked.connect(self.get_text)
+        self.enter_2.clicked.connect(self.get_rarity)
 
         self.build.clicked.connect(build_)
         self.install.clicked.connect(install_)
 
         self.remove_layer.clicked.connect(remove_layer_)
         self.add_layer.clicked.connect(lambda: add_layer_(self.layer_name))
+
+        self.add_rarity.clicked.connect(lambda: change_rarity_(self.rarity))
 
         self.add_image.clicked.connect(add_file_)
         self.remove_image.clicked.connect(remove_file_)
@@ -31,15 +31,17 @@ class doctors_nft(QtWidgets.QMainWindow, ui_pyqt.Ui_MainWindow):
         self.layer_name = self.enter_text.toPlainText()
         self.enter_text.clear()
 
+    def get_rarity(self):
+        self.rarity = self.enter_text_2.toPlainText()
+        self.enter_text_2.clear()
+
 
 def main():
-    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
-    window = doctors_nft()  # Создаём объект класса ExampleApp
-    window.show()  # Показываем окно
-    app.exec_()  # и запускаем приложение
+    app = QtWidgets.QApplication(sys.argv)
+    window = doctors_nft()
+    window.show()
+    app.exec_()
 
 
-if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
-    while True:
-        main()  # то запускаем функцию main()
-
+if __name__ == '__main__':
+    main()
